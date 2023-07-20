c71c5146726df542b57d1e060dbad78b52b2f035
Viktor <93682529+savage-viktor@users.noreply.github.com>
2023-07-20 22:30:58 +0300

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,39 +10,24 @@ const modalRoot = document.querySelector("#modal");
 class Modal extends Component {
   componentDidMount() {
     window.addEventListener("keydown", this.listener);
-    // window.addEventListener("click", this.listener);
+    modalRoot.addEventListener("click", this.listener);
   }
 
   componentWillUnmount() {
     window.removeEventListener("keydown", this.listener);
+    modalRoot.removeEventListener("click", this.listener);
   }
 
   listener = (event) => {
-    if (event.code === "Escape") {
+    if (event.code === "Escape" || event.target.id === "modal-overlay") {
       this.props.onClose();
     }
-    // console.log(event.target);
-    // console.log(event.currentTarget);
-    // if (event.target !== event.currentTarget) {
-    //   console.log("Клікнули мимо");
-    // }
   };
-
-  // render() {
-  //   return createPortal(
-  //     <div className={css.overlay} onClick={this.props.toggleModal}>
-  //       <div className={css.modal}>
-  //         <img src={this.props.modalImage} alt={this.props.alt} width="700" />
-  //       </div>
-  //     </div>,
-  //     modalRoot
-  //   );
-  // }
 
   render() {
     const { image, description } = this.props;
     return createPortal(
-      <div className={styles.overlay}>
+      <div id="modal-overlay" className={styles.overlay}>
         <div className={styles.modal}>
           <img src={image} alt={description} />
         </div>
